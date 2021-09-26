8aeee555aff50df45d6dc8ebab53c6f77308835b
Carlos Martinez <curlosmart@gmail.com>
2021-09-26 16:27:01 -0400

    product model

---
+++ server/models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {type: String, required: true},
+    category: {type: String, required: true},
+    retailPrice: {type: Number, required: true},
+    releaseYear: {type: Number},
+    capacity: {type: String},
+    color: {type: String},
+    imageURL: {type: String},
+  }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product;
